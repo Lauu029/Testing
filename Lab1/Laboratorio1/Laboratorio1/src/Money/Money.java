d3ef7fe1677bfce86fbb5caacb1084ec99ada0f8
Laura <lgomez25@ucm.es>
2022-09-23 19:32:04 +0200

    la practica 1 es

---
+++ Lab1/Laboratorio1/Laboratorio1/src/Money/Money.java
@@ -0,0 +1,96 @@
+package Money;
+
+public class Money implements Comparable {
+	
+	private int cantidad;
+	private Divisa divisa;
+
+	/**
+	 * @param cantidad	
+	 * @param divisa	
+	 */
+	Money (int cantidad, Divisa divisa) {
+		this.cantidad = cantidad;
+		this.divisa = divisa;
+	}
+	
+	/**
+	 * Return Cantidad.
+	 * @return Cantidad.
+	 */
+	public int getCantidad() {
+		return this.cantidad;
+	}
+	
+	/**
+	 * Returns Divisa.
+	 * @return Divisa asociada a esta cantidad
+	 */
+	public Divisa getDivisa() {
+		return this.divisa;
+	}
+	
+	/**
+	 * Devuelve un String "cantidad nombre de divisa", e.g. "10.5 SEK".
+	 * Los numeros decimales se representan con enteros, e.g "10.5 SEK" se almacena como 1050
+	 * 
+	 *  @return String con informaci�n de la moneda.
+	 */
+	public String toString() {
+		
+	}
+	
+	/**
+	 * Valor universal de la cantidad de acuerdo al rate asociado a la divisa
+	 * @return Valor universal de la cantidad
+	 */
+	public int valorUniversal() {
+		
+	}
+	
+	/**
+	 * Chequea si el valor universal de otro objeto Money es equivalente al actual
+	 * @param Objeto Money a comparar
+	 * @return Boolean indicando equivalencia
+	 */
+	public Boolean equals(Money otra) {
+		
+	}
+	
+	/**
+	 * A�ade un objeto Money al actual
+	 * @param Objeto Money a a�adir.
+	 * @return Un nuevo objeto Money con la divisa del actual, y la cantidad acumulada con el importe del objeto Money recibido.
+	 **/
+	public Money add(Money otra) {
+		return (new Money(this.cantidad + divisa.valorEnEstaDivisa (otra.cantidad, otra.divisa),this.divisa));
+	}
+
+		
+	/**
+	 * 
+	 * @return True si cantidad es cero, false e.o.c.
+	 */
+	public Boolean isZero() {
+		return (this.cantidad==0);
+
+	}
+	/**
+	 * Convierte la cantidad del objeto actual a negativo
+	 * @return Un nuevo objeto Money con la cantidad en negativo
+	 */
+	public Money negate() {
+		return (new Money(cantidad*-1,this.divisa));
+	}
+	
+	/**
+	 * Compara dos objetos Money
+	 * @return 0  si sus valores son iguales
+	 * Un entero negativo si el objeto actual tiene menos valor que el recibido
+	 * Un entero positivo si el objeto actual tiene m�s valor que el recibido
+	 */
+	public int compareTo(Object otra) {
+			
+		
+	}
+}
