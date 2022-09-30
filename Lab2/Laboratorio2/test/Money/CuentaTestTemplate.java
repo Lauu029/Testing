c89fbcee8d6e0d4fa5b84e3ddc806be8bed6d728
SergioJAlfonso <sergialf@ucm.es>
2022-09-30 15:42:46 +0200

    Ligeramente pelícano

--- Lab2/Laboratorio2/test/Money/CuentaTestTemplate.java
+++ Lab2/Laboratorio2/test/Money/CuentaTestTemplate.java
@@ -5,56 +5,70 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import org.junit.runner.RunWith;
+//import junitparams.JUnitParamsRunner;
+//import junitparams.Parameters;
 
 import static org.junit.Assert.*;
 
 public class CuentaTestTemplate {
 	
 	Divisa SEK, DKK;
-	Banco Nordea;
-	Banco DanskeBanco;
-	Banco SweBanco;
-	Cuenta testAccount;
+	Banco Nordea, DanskeBanco, SweBanco;
+	Cuenta testAccount1, testAccount2;
 	Money importe;
 	
 	@Before
 	public void setUp() throws Exception {
 		
 		SEK = new Divisa("SEK", 0.15);
-		SweBanco = new Banco("SweBanco", SEK);
-		
+		DKK = new Divisa("DKK", 1.5);
+		testAccount1 = new Cuenta("testAccount1", SEK);
+		testAccount2 = new Cuenta("testAccount2", DKK);	
+		testAccount1.pagoPeriodico("dos cheeseburguers", new Money(10,DKK), testAccount2);
 	}
-	
+//	private static final Object[] getCuenta() {
+//		return new Object[] {
+//				new Object[] {"testAccount1", "SEK", 0.15},
+//				new Object[] {"testAccount2", "EUR", 1.5}		
+//		};
+//	}
 
-	@Test
+	
 	public void testGetSaldo() {
-		fail("No implementado");
+		assertEquals("Concha", testAccount1.getSaldo(), new Money(0,SEK));
+		assertEquals("Concha", testAccount2.getSaldo(), new Money(0,DKK));
 	}
 	
 		
 	@Test
 	public void testAnadirPagoPeriodico() throws PagoExistenteException{
-		fail("No implementado");
+		testAccount1.pagoPeriodico("pa misiles de siria", new Money(10,DKK), testAccount2);
+		assertTrue(testAccount1.pagoPeriodicoExiste("pa misiles de siria"));
 	}
 	
-	@Test
+	@Test(expected = PagoExistenteException.class)
 	public void testAnadirPagoPeriodicoException() throws PagoExistenteException{
-		fail("No implementado");
+			testAccount1.pagoPeriodico("pa misiles de siria", new Money(10,DKK), testAccount2);
+			testAccount1.pagoPeriodico("pa misiles de siria", new Money(10,DKK), testAccount2);
 	}
 	
 	@Test
 	public void testBorrarPagoPeriodico()  throws PagoNoExistenteException, PagoExistenteException {
-		fail("No implementado");
+		testAccount1.pagoPeriodico("aguanta", new Money(10,DKK), testAccount2);
+		assertTrue(testAccount1.pagoPeriodicoExiste("aguanta"));
+		testAccount1.cancelarPagoPeriodico("aguanta");
+		assertFalse(testAccount1.pagoPeriodicoExiste("aguanta"));
 	}
 	
-	@Test
+	@Test(expected = PagoNoExistenteException.class)
 	public void testBorrarPagoPeriodicoException()  throws PagoNoExistenteException {
-		fail("No implementado");
+		testAccount1.cancelarPagoPeriodico("ahuevo");
 	}
 	
 	@Test
 	public void testPagoPeriodicoExiste() throws PagoNoExistenteException,PagoExistenteException{
-		fail("No implementado");
+		assertTrue(testAccount1.pagoPeriodicoExiste("dos cheeseburguers"));
 	}
 	
 	@Test
@@ -67,3 +81,4 @@ public class CuentaTestTemplate {
 		fail("No implementado");
 	}
 }
+
