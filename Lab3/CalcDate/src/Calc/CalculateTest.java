63de3ec4afe9fa6fd2edee5fade411ca09d45438
SergioJAlfonso <sergialf@ucm.es>
2022-10-07 15:29:56 +0200

    no hemos hecho mas metodos, merche

--- Lab3/CalcDate/src/Calc/CalculateTest.java
+++ Lab3/CalcDate/src/Calc/CalculateTest.java
@@ -15,7 +15,10 @@ public class CalculateTest {
 	@Test
 	public void testCalc() {
 		assertEquals("test 1", cl.cal(1,2,1,3,2002), 1);
-		assertEquals("test 2", cl.cal(3,3,4,4,1987), 32);
+		assertEquals("test 2", cl.cal(3,3,5,4,1987), 62);
 		assertEquals("test 3", cl.cal(3,3,6,4,2000), 93);
+		assertEquals("test 4", cl.cal(3,3,4,4,2000), 32);
+		assertEquals("test 5", cl.cal(3,3,4,4,1987), 32);
 	}
+	
 }
