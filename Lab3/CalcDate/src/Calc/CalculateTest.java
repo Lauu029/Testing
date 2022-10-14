b1417aadaa587c2315fbd6ed61e67ea902245c5a
SergioJAlfonso <sergialf@ucm.es>
2022-10-14 15:38:59 +0200

    mas

--- Lab3/CalcDate/src/Calc/CalculateTest.java
+++ Lab3/CalcDate/src/Calc/CalculateTest.java
@@ -14,28 +14,42 @@ public class CalculateTest {
 	
 	@Test
 	public void testCalc1() {
-		assertEquals("test 1", cl.cal(1,2,1,3,2002), 1);
+		//1,2,3
+		assertEquals("test 1", 1,  cl.cal(1,2,1,3,2002));
 	}
 	@Test
 	public void testCalc2() {
-		assertEquals("test 2", cl.cal(3,3,5,4,1987), 62);
-		
+		assertEquals("test 2", 62, cl.cal(3,3,5,4,1987));	
 	}
 	@Test
 	public void testCalc3() {
-		assertEquals("test 3", cl.cal(3,3,6,4,2000), 93);
-		
+		assertEquals("test 3", 93, cl.cal(3,3,6,4,2000));		
 	}
 	@Test
 	public void testCalc4() {
-		assertEquals("test 4", cl.cal(3,3,4,4,2000), 32);
-		
+		assertEquals("test 4", 32, cl.cal(3,3,4,4,2000));		
 	}
 	@Test
 	public void testCalc5() {
-		assertEquals("test 5", cl.cal(3,3,4,4,1987), 32);
-		
+		assertEquals("test 5", 32, cl.cal(3,3,4,4,1987));		
 	}
 	
+	@Test
+	public void testCalc6() {
+		//mt3
+		assertEquals("test 6", 32, cl.cal(1,3,2,4,1987));
+	}
 	
-}
+	@Test
+	public void testCalc7() {
+		//mt4
+		assertEquals("test 7", 61, cl.cal(1,3,3,4,2000));		
+	}
+	
+	@Test
+	public void testCalc8() {
+		//mt5
+		assertEquals("test 8", 60, cl.cal(1,3,3,4,1000));		
+	}
+	
+}<>
