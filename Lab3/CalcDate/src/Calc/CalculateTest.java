c5e1af5ead6ad8f1e698525c4ec682cf3dea8639
SergioJAlfonso <sergialf@ucm.es>
2022-10-14 14:19:07 +0200

    Update CalculateTest.java

--- Lab3/CalcDate/src/Calc/CalculateTest.java
+++ Lab3/CalcDate/src/Calc/CalculateTest.java
@@ -13,12 +13,29 @@ public class CalculateTest {
 	}
 	
 	@Test
-	public void testCalc() {
+	public void testCalc1() {
 		assertEquals("test 1", cl.cal(1,2,1,3,2002), 1);
-		assertEquals("test 2", cl.cal(3,3,5,4,1987), 62);
-		assertEquals("test 3", cl.cal(3,3,6,4,2000), 93);
-		assertEquals("test 4", cl.cal(3,3,4,4,2000), 32);
-		assertEquals("test 5", cl.cal(3,3,4,4,1987), 32);
 	}
+	@Test
+	public void testCalc2() {
+		assertEquals("test 2", cl.cal(3,3,5,4,1987), 62);
+		
+	}
+	@Test
+	public void testCalc3() {
+		assertEquals("test 3", cl.cal(3,3,6,4,2000), 93);
+		
+	}
+	@Test
+	public void testCalc4() {
+		assertEquals("test 4", cl.cal(3,3,4,4,2000), 32);
+		
+	}
+	@Test
+	public void testCalc5() {
+		assertEquals("test 5", cl.cal(3,3,4,4,1987), 32);
+		
+	}
+	
 	
 }
