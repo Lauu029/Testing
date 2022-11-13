7fda5f55f2517ba7778591b9d7b08f4c7fff4d4d
SergioJAlfonso <sergialf@ucm.es>
2022-11-13 11:58:52 +0100

    Todo el inicio

--- Lab2/Laboratorio2/el6/B/Cliente.java
+++ Lab2/Laboratorio2/el6/B/Cliente.java
@@ -2,19 +2,19 @@ package B;
 
 public class Cliente {
  
- private Boolean opt;
- private String email;
- 
- public Cliente(String email, Boolean opt) {
-	 this.email=email;
-	 this.opt=opt;
- }
- public Boolean prefiereEmails() {
-	 return this.opt;
- };
- 
- 
- public String getEmail() {
-	 return this.email;
- };
+	 private Boolean opt;
+	 private String email;
+	 
+	 public Cliente(String email, Boolean opt) {
+		 this.email=email;
+		 this.opt=opt;
+	 }
+	 public Boolean prefiereEmails() {
+		 return this.opt;
+	 };
+	 
+	 
+	 public String getEmail() {
+		 return this.email;
+	 };
 }
--- Lab2/Laboratorio2/el6/B/TemplateEngine.java
+++
@@ -1,7 +0,0 @@
-package B;
-
-public interface TemplateEngine {
-	
-	String preparaMensaje(Template template, Cliente cliente);
-	
-}
