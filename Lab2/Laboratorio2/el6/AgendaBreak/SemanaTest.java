c7fd3392fd33263a13d1f12bcd1a62b6cd3d0ea7
danlles247 <dailla01@ucm.es>
2022-12-13 16:48:10 +0100

    s1 por el culo te la hinco

--- Lab2/Laboratorio2/el6/AgendaBreak/SemanaTest.java
+++ Lab2/Laboratorio2/el6/AgendaBreak/SemanaTest.java
@@ -6,14 +6,41 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SemanaTest {
+	Semana s;
 	@Before
 	public void setUp() throws Exception {
 
 	}
 	
 	@Test
+	public void testSemana() throws DatoException{
+		//p1
+		assertThrows(DatoException.class, () -> s = new Semana(0)); //T F
+		assertThrows(DatoException.class, () -> s = new Semana(54)); //F T
+		
+		s = new Semana(2);
+		assertEquals("s1 -> a = F, b = F", 2, s.getNumeroSemana());
+		
+		//p2
+		assertEquals("s1 -> c = T", 2, s.getNumeroSemana());
+		s = new Semana(1);
+		assertEquals("s1 -> c = F", 1, s.getNumeroSemana());
+		
+		//p3
+		s = new Semana(24);
+		assertEquals("s1 -> d = T, e = T", 24, s.getNumeroSemana());
+		s = new Semana(25);
+		assertEquals("s1 -> d = T, e = F", 25, s.getNumeroSemana());
+		s = new Semana(53);
+		assertEquals("s1 -> d = F, e = T", 53, s.getNumeroSemana());
+		s = new Semana(1);
+		assertEquals("s1 -> d = F, e = F", 1, s.getNumeroSemana());
+
+	}
+	
+	@Test
 	public void testDiaSemana() throws DatoException {	
-		Semana s = new Semana(0);
+		s = new Semana(1);
 		
 		assertEquals("Should be No citable", "No citable", s.diaSemana(0));
 		assertEquals("Should be Lunes", "Lunes", s.diaSemana(1));
